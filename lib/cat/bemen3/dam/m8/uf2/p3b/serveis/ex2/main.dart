4798d62b01ce33734f4d7ff970a00ccb826b3320
YvngS1mp <hoacme23@bemen3.cat>
2025-03-12 19:26:07 +0100

    Exercici 4

--- lib/cat/bemen3/dam/m8/uf2/p3b/serveis/ex2/main.dart
+++ lib/cat/bemen3/dam/m8/uf2/p3b/serveis/ex2/main.dart
@@ -20,8 +20,8 @@ class MainApp extends StatelessWidget {
       title: 'Provider Counter',
       theme: ThemeData(
         primarySwatch: Colors.purple,
-      ), // ThemeData
+      ),
       home: const HomeScreen(),
-    ); // MaterialApp
+    );
   }
 }
